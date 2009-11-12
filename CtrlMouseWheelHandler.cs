76d63073284563ecf71f7e591260b54b59fd0493
Noah Richards <noahric@microsoft.com>
2009-11-11 23:33:53 -0800

    Added CtrlMouseWheelHandler.cs.

---
+++ CtrlMouseWheelHandler.cs
@@ -0,0 +1,59 @@
+﻿// CtrlMouseWheelHandler - Handler Ctrl+mouse wheel events to scroll by pages instead of lines
+
+using System;
+using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CtrlMouseWheel
+{
+    [Export(typeof(IMouseProcessorProvider))]
+    [Name("Ctrl Mouse Processor")]
+    [Order(Before = "Zoom")] // Make sure to pre-empt the Zoom mouse processor in handling ctrl+wheel
+    [ContentType("any")]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    class MouseProcessorProvider : IMouseProcessorProvider
+    {
+        public IMouseProcessor GetAssociatedProcessor(IWpfTextView wpfTextView)
+        {
+            return new CtrlMouseProcessor(wpfTextView);
+        }
+    }
+
+    class CtrlMouseProcessor : MouseProcessorBase
+    {
+        ITextView _view;
+
+        int currentDelta = 0;
+
+        public CtrlMouseProcessor(ITextView view)
+        {
+            _view = view;
+        }
+
+        public override void PreprocessMouseWheel(MouseWheelEventArgs e)
+        {
+            // Only handle this when the ctrl key is held down
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == 0)
+                return;
+
+            currentDelta += e.Delta;
+
+            // Check to see if we've accumulated enough clicks
+            int pagesToScroll = currentDelta / Mouse.MouseWheelDeltaForOneLine;
+            if (pagesToScroll != 0)
+            {
+                currentDelta -= (pagesToScroll * Mouse.MouseWheelDeltaForOneLine);
+
+                // A negative value means to scroll down, positive up
+                ScrollDirection direction = pagesToScroll < 0 ? ScrollDirection.Down : ScrollDirection.Up;
+
+                for (int i = Math.Abs(pagesToScroll); i > 0; i--)
+                    _view.ViewScroller.ScrollViewportVerticallyByPage(direction);
+            }
+
+            e.Handled = true;
+        }
+    }
+}
---
+++ README.markdown
@@ -0,0 +1,8 @@
+Some random examples of editor extension points, usually for answering a question online.
+
+CtrlMouseWheelHandler.cs
+------------------------
+
+In response to a [question from SonoranCellist on twitter](http://twitter.com/SonoranCellist/status/5643305534).
+
+This is a simple extension for turning ctrl+mouse wheel events into scrolling by page instead of scrolling by line (or zooming, since that is the key combination for the zoom control).  Answers a question asked on the Visual Studio forums back in 2005: <http://social.msdn.microsoft.com/Forums/en-US/vsx/thread/c859e7c9-5205-402e-ac9e-2938382ef5ef/>.
